7d470873f57507a0f25bc3d616e2c40b6f37731f
lrh <837233792@qq.com>
2020-01-25 16:58:56 +0800

    错误异常捕获

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App'
+import MpvueRouterPatch from 'mpvue-router-patch'
+import './style/base.scss'
+import { errorHandler, onError } from './utils/error'
 
+Vue.use(MpvueRouterPatch)
 Vue.config.productionTip = false
 App.mpType = 'app'
+
+// Vue异常捕获
+Vue.config.errorHandler = errorHandler
+console.log('mpvue', mpvue)
+console.log('mpvuePlatform', mpvuePlatform)
+
+if (mpvuePlatform === 'my') {
+  // do something...
+} else {
+  // App全局异常捕获
+  mpvue.onError((err) => onError(err, app))
+}
 
 const app = new Vue(App)
 app.$mount()
--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './index'
-import MpvueRouterPath from 'mpvue-router-patch'
-
-Vue.use(MpvueRouterPath)
-Vue.config.productionTip = false
-// App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
